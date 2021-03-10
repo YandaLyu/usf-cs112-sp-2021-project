cdf83ffa7332973820d725a5bebef885be722228
YandaLyu <80372498+YandaLyu@users.noreply.github.com>
2021-03-09 19:15:25 -0800

    Create Pokemonn.java

---
+++ Pokemon_YandaLyu/Pokemonn.java
@@ -0,0 +1,35 @@
+package SchoolDemo1;
+
+//父类
+
+public class Pokemonn {
+    private String name;
+    private String element;
+    private String ability;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+
+}
